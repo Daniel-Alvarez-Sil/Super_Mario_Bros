793cf22fdce5eeae613bcc2e9ecb2e853396d275
Daniel Alvarez Sil <A01800182@tec.mx>
2025-03-22 20:07:05 -0600

    Game and Credits Finished without Return button

---
+++ Assets/Scripts/ControladorMenu.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class ControladorMenu : MonoBehaviour
+{
+    private UIDocument menu;
+    private Button botonJugar;
+    private Button botonAyuda;
+    private Button botonCreditos;
+    // private Button botonSalir;
+    private VisualElement vMenu;
+
+    void OnEnable()
+    {
+        menu = GetComponent<UIDocument>();
+        var root = menu.rootVisualElement;
+
+        // Referencias a los botones
+        botonJugar = root.Q<Button>("boton-juego");
+        botonAyuda = root.Q<Button>("boton-ayuda");
+        botonCreditos = root.Q<Button>("boton-creditos");
+        // botonSalir = root.Q<Button>("close-help");
+
+        // Referencia al menú de ayuda
+        vMenu = root.Q<VisualElement>("Menu");
+
+        // Asignación de eventos de clic
+        botonJugar.RegisterCallback<ClickEvent>(evt => CargarEscena("Game"));
+        botonAyuda.RegisterCallback<ClickEvent>(evt => MostrarAyuda());
+        botonCreditos.RegisterCallback<ClickEvent>(evt => CargarEscena("credits"));
+        // botonSalir.RegisterCallback<ClickEvent>(evt => CerrarAyuda());
+    }
+
+    private void CargarEscena(string nombreEscena)
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+
+   private void MostrarAyuda()
+{
+    vMenu.style.display = DisplayStyle.Flex;
+    vMenu.Q<Label>("help-text").text = "¡Hola! Bienvenido al menú de ayuda del juego de Mario Bros.\n\n" +
+        "Este es un juego creado en Unity para practicar el uso de funciones básicas, interfaces gráficas y elementos interactivos.\n\n" +
+        "El objetivo principal es experimentar con el motor de Unity y aprender a crear juegos sencillos, utilizando conceptos fundamentales de diseño y programación.\n\n" +
+        "¡Explora los niveles, recolecta monedas y enfréntate a los clásicos desafíos de Mario!\n\n" +
+        "¡Diviértete y disfruta de la experiencia!";
+}
+
+
+    // private void CerrarAyuda()
+    // {
+    //     vMenu.style.display = DisplayStyle.None;
+    // }
+}
--- Assets/Scripts/MuevePersonaje.cs
+++ Assets/Scripts/MuevePersonaje.cs
@@ -2,27 +2,31 @@ using UnityEngine;
 
 public class MuevePersonaje : MonoBehaviour
 {
-    //Velocidad
+    // transform, gameObject, 
+
+    // Velocidades
     public float velocidadX;
 
-    [SerializeField] // Permiso a Unity de acceder a la variable
+    [SerializeField]   // Permite al editor de Unity acceder a la var
     private float velocidadY;
 
-    // Rigidbody para usar la física
+    // Necesito que el personaje se mueva
     private Rigidbody2D rb;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Inicializar rb
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    // FixedUpdate() is called 50 times per second
-    void Update()
+    // FixUpdate is called 60 fps
+    void FixedUpdate()
     {
         float movHorizontal = Input.GetAxis("Horizontal");
         float movVertical = Input.GetAxis("Vertical");
+
         if (movVertical > 0) {
             rb.linearVelocity = new Vector2(movHorizontal*velocidadX, movVertical*velocidadY);
         } else {
